6ca4be2401a06e1edcb6af70ce2d1f4bf49ef3fc
skwaweru254 <skwaweru254@gmail.com>
2022-12-09 15:55:50 +0300

    Convert csv to excel using few lines

--- import_pandas.py
+++ import_pandas.py
@@ -9,3 +9,17 @@ excel_data = pd.read_excel('/home/skwaweru254/Excel/salesContacts.xlsx', usecols
 data = pd.DataFrame(excel_data)
 
 print(data)
+
+
+#   Convert csv to excel using few lines
+cols1 = [1, 2]
+
+csv_data = pd.read_csv('/home/skwaweru254/passwords.csv', usecols=cols1)
+
+df = pd.DataFrame(csv_data)
+
+convert_excel = csv_data.to_excel('/home/skwaweru254/passwords.xlsx')
+
+print(df)
+
+print(convert_excel)
